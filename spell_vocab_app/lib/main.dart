b75b2b72dff2cc91005a20688285ffff617db9ef
Aaron McKain <maaron41602@gmail.com>
2023-06-21 13:04:29 -0500

    Buttons now correspond with proper pages

--- spell_vocab_app/lib/main.dart
+++ spell_vocab_app/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'My Spelling App',
       home: HomeScreen(),
     );
@@ -15,6 +17,8 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -24,23 +28,40 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Spelling App'),
+        title: const Text('My Spelling App'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-              child: Text('Spelling Test'),
-              onPressed: () {},
+              child: const Text('Spelling Test'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SpellingTestScreen()),
+                );
+              },
             ),
             ElevatedButton(
-              child: Text('Vocabulary Test'),
-              onPressed: () {},
+              child: const Text('Vocabulary Test'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const VocabularyTestScreen()));
+              },
             ),
             ElevatedButton(
-              child: Text('Word Games'),
-              onPressed: () {},
+              child: const Text('Word Games'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const WordGamesScreen()),
+                );
+              }, //On Pressed
             ),
           ],
         ),
@@ -50,12 +71,46 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 class SpellingTestScreen extends StatelessWidget {
+  const SpellingTestScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Spelling Test'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Spelling Test'),
+        ),
+        body: const Center(
+          child: Text('Spelling Test Screen'),
+        ));
+  }
+}
+
+class VocabularyTestScreen extends StatelessWidget {
+  const VocabularyTestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Vocabulary Test'),
+        ),
+        body: const Center(
+          child: Text('Vocabulary Test Screen'),
+        ));
+  }
+}
+
+class WordGamesScreen extends StatelessWidget {
+  const WordGamesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Word Games'),
+        ),
+        body: const Center(
+          child: Text('Word Games Screen'),
+        ));
   }
 }
